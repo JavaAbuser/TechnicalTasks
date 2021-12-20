5550610253d8528f2e41b199b02b3dbd45820d7a
Max <43775453+JavaAbuser@users.noreply.github.com>
2021-12-20 14:10:56 +0200

    Update Main.java
    adddint @author

--- JetBulb/Main.java
+++ JetBulb/Main.java
@@ -1,5 +1,7 @@
 package JetBulb;
-
+/**
+ * @author Max Steblevskiy(JavaAbuser) on 20.12.21
+ */
 public class Main {
     public static void main(String[] args) {
         NotationConverter converter = new NotationConverter();
