17389b74704cdb0a8e48813b5810a311693ae1be
Max <43775453+JavaAbuser@users.noreply.github.com>
2022-03-14 20:00:24 +0200

    add logic to Task3Sum.java

--- DigitalEconomyLeague/main/java/ru/rtech/internship/Task3Sum.java
+++ DigitalEconomyLeague/main/java/ru/rtech/internship/Task3Sum.java
@@ -11,7 +11,21 @@ public class Task3Sum {
      * Функция должна вернуть результат выражения. Скобки учитывать не надо. Между числами и знаками может быть разное количество пробелов. Если во время
      * вычисления случилась ошибка и выражение вычислить не получается - вернуть null.
      */
-    public Integer sum(String experssion){
-        return 9;
+    public Integer sum(String expression){
+        int result = 0;
+        String str = expression.replaceAll("\\s*", "");
+        int counter = 0;
+        for(int i = 0; i < str.toCharArray().length; i++){
+            if(str.charAt(i) == '+' || str.charAt(i) == '-'){
+                if(str.charAt(i) == '+'){
+                    result += Integer.parseInt(str.subSequence(counter, i).toString());
+                    counter = i + 1;
+                } else if(str.charAt(i) == '-'){
+                    result -= Integer.parseInt(str.subSequence(counter, i).toString());
+                    counter = i + 1;
+                }
+            }
+        }
+        return result;
     }
 }
